1bbb19c261d599fbb8b7d32d64ff972b829c7fa8
Róger Hoffmann <rogerhival@gmail.com>
2015-05-18 17:17:15 -0300

    Update app.js

--- app.js
+++ app.js
@@ -89,6 +89,17 @@ app.use('/api/', router);
       // respond with 500 "Internal Server Error".
       res.sendStatus(500);
   });
+  
+  var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+app.use(allowCrossDomain);
+
 
 //   __  __    __    ____  _  _
 //  (  \/  )  /__\  (_  _)( \( )
